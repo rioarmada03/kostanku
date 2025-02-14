ab9e741afa2e135c5697010fc75565e64a57bf8d
rioarmada03 <rioarmada03@gmail.com>
2025-02-15 01:15:04 +0700

    Menambahkan fitur pencarian kost002

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,15 @@
 
 use App\Http\Controllers\KosController;
 
+
 Route::get('/', [KosController::class, 'index'])->name('home');
-Route::get('/tentang-kami', function () {
+Route::get('/about', function () {
     return view('about');
 })->name('about');
-Route::get('/kontak', function () {
+Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
-Route::get('/tambah-kos', [KosController::class, 'create'])->name('addKos');
-Route::post('/tambah-kos', [KosController::class, 'store'])->name('storeKos');
+Route::get('/add-kos', [KosController::class, 'create'])->name('addKos');
+Route::post('/add-kos', [KosController::class, 'store'])->name('storeKos');
 Route::post('/search', [KosController::class, 'search'])->name('search');
 Route::get('/route/{id}', [KosController::class, 'showRoute'])->name('route');
