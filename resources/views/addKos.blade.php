7e39d82438090bebf488b6dce7fdbd9022fe9ba0
rioarmada03 <rioarmada03@gmail.com>
2025-02-28 11:05:49 +0700

    fix18

--- resources/views/addKos.blade.php
+++ resources/views/addKos.blade.php
@@ -12,7 +12,7 @@
         <p style="color: green;">{{ session('success') }}</p>
     @endif
 
-        <form method="POST" action="{{ route('store', [], true) }}">
+        <form method="POST" action="{{ route('storeKos', [], true) }}">
 
         @csrf
 
